6f5e722365ffd04d526139baa01f125f0a763eb2
nhaxxesg <96924082+nhaxxesg@users.noreply.github.com>
2025-05-12 08:59:32 -0500

    add fields in product and controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,37 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('products.index');
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        // Logic to store the product
+        
+        return redirect()->route('products.index');
+    }
+
+    public function edit($id)
+    {
+        return view('products.edit', compact('id'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Logic to update the product
+        return redirect()->route('products.index');
+    }
+
+    public function destroy($id)
+    {
+        // Logic to delete the product
+        return redirect()->route('products.index');
+    }
 }
